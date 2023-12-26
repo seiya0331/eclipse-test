45e8acf1ea54b675b30da03a3732ee9ba6003512
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-12-26 18:11:21 +0900

    練習問題

---
+++ src/Chapter05.java
@@ -0,0 +1,17 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str = "山口聖也";
+		System.out.println(str);
+		
+		str = "スパイダーマン";
+		System.out.println(str);
+		
+		final int luckyNumber = 7;
+		System.out.println(luckyNumber);
+
+	}
+
+}
